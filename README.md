af53dd62372d4b9c0f91260e2e286be76a1b1d1b
Yevhenii Pletenskyi <yevhenii.p@thewhitelabelagency.com>
2024-11-01 21:09:49 +0200

    resolved the build issue

---
+++ README.md
@@ -0,0 +1,32 @@
+# Snake Game by Yevhenii Pletenskyi
+
+
+## Live Demo
+
+You can view the live demo of the project at [https://zesty-sopapillas-d1f81f.netlify.app/](https://zesty-sopapillas-d1f81f.netlify.app/).
+
+## Getting Started
+
+These instructions will help you set up the project locally on your machine for development and testing purposes.
+
+### Prerequisites
+
+Make sure you have the following installed on your machine:
+
+- [Node.js](https://nodejs.org/)
+- [npm](https://www.npmjs.com/)
+
+### Installing
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/yourusername/your-repo-name.git
+   
+2. Navigate into the project directory:
+
+   cd your-repo-name
+   
+3. Install the necessary dependencies:
+   ```bash
+    npm run dev
--- main.js
+++ main.js
@@ -1,28 +1,30 @@
 import { Application } from 'pixi.js';
 import Game from './src/classes/Game.js';
 
-const app = new Application();
-await app.init({ width: 740, height: 440, backgroundColor: 0x575757 });
-// globalThis.__PIXI_APP__ = app;
+(async () => {
+  const app = new Application();
+  await app.init({ width: 740, height: 440, backgroundColor: 0x575757 });
+  // globalThis.__PIXI_APP__ = app;
 
-document.getElementById('game-container').appendChild(app.canvas);
+  document.getElementById('game-container').appendChild(app.canvas);
+  console.log('build');
 
-const game = new Game(app);
+  const game = new Game(app);
 
-let counter = 0;
-app.ticker.add((ticker) => {
-  counter += ticker.elapsedMS;
-  if (counter > game.speed) {
-    if (game.gameStatus === 'start') {
-      game.initGameObject();
-      game.menu.gameStatus = 'playing';
+  let counter = 0;
+  app.ticker.add((ticker) => {
+    counter += ticker.elapsedMS;
+    if (counter > game.speed) {
+      if (game.gameStatus === 'start') {
+        game.initGameObject();
+        game.menu.gameStatus = 'playing';
+      }
+      if (game.gameStatus === 'playing') {
+        game.update();
+        counter = 0;
+      } else {
+        game.reset();
+      }
     }
-    if (game.gameStatus === 'playing') {
-      game.update();
-      counter = 0;
-    }
-    else {
-      game.reset();
-    }
-  }
-});
+  });
+})();
